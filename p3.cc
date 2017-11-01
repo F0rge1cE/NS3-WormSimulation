6715f491e96598218713ce215760a05a0cef12e7
DoodleNan <doodlenan@outlook.com>
2017-11-01 15:34:40 -0400

    Create Worm model for simulation. Still need to be tested :(

--- p3.cc
+++ p3.cc
@@ -2,25 +2,25 @@
 
 #include "p2pCampusHelper.h"
 
+
 int main(int argc, char** argv) {
-	
-	SeedManager::SetSeed(1);
-	uint32_t nInner = 8;
-	uint32_t nChild = 2;
+    SeedManager::SetSeed(1);
+    uint32_t nInner = 8;
+    uint32_t nChild = 2;
     double endTime = 60.0;
 
     CommandLine cmd;
-  	cmd.AddValue("nInner", "Number of inner nodes in each bomb", nInner);
-  	cmd.AddValue("nChild", "Number of child nodes in each bomb", nChild);
-  	cmd.Parse(argc, argv);
+    cmd.AddValue("nInner", "Number of inner nodes in each bomb", nInner);
+    cmd.AddValue("nChild", "Number of child nodes in each bomb", nChild);
+    cmd.Parse(argc, argv);
 
-  	PointToPointHelper hubInner;
+    PointToPointHelper hubInner;
     hubInner.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
     hubInner.SetChannelAttribute("Delay", StringValue("5ms"));
-	
-	PointToPointHelper innerChild;
-	innerChild.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-	innerChild.SetChannelAttribute("Delay", StringValue("8ms"));
+
+    PointToPointHelper innerChild;
+    innerChild.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+    innerChild.SetChannelAttribute("Delay", StringValue("8ms"));
 
     p2pCampusHelper bomb(nInner, hubInner, nChild, innerChild);
     InternetStackHelper stack;
@@ -29,11 +29,12 @@ int main(int argc, char** argv) {
     bool IsValidInnerNode [nInner];
     for(uint32_t i = 0; i < nInner; ++i) IsValidInnerNode[i] = true;
     bool IsValidChildNode [nInner * nChild];
-	for(uint32_t i = 0; i < nInner * nChild; ++i) IsValidChildNode = true;
+    for(uint32_t i = 0; i < nInner * nChild; ++i) IsValidChildNode = true;
 
-	Ipv4AddressHelper address;
+    Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
     bomb.AssignIpv4Addresses(address);
-	std::cout << "Hello" << std::endl; 
+    
+	  std::cout << "Hello" << std::endl; 
 
 }
---
+++ worm.h
@@ -0,0 +1,86 @@
+#include "ns3/core-module.h"
+#include "ns3/application.h"
+#include "ns3/internet-module.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/socket.h"
+#include "ns3/event-id.h"
+#include "ns3/data-rate.h"
+#include "ns3/traced-callback.h"
+
+#include <vector>
+
+//NS_LOG_COMPONENT_DEFINE ("ns3-worm");
+
+/**
+ * \class Worm Worm.h
+ * \brief Worm application
+ */
+
+ class Worm : public ns3::Application{
+ public:
+ 	static ns3::TypeId GetTypeId(void);
+ 	Worm();
+ 	~Worm();
+
+ 	void SetUp(std::string protocol, uint32_t infectionPort);
+
+ 	void SetInfected(bool isInfected);
+ 	
+ 	ns3::Ipv4Address generateIP(void);
+
+
+
+ protected:
+ 	static uint32_t m_totalInfected;
+ 	static uint32_t m_totalNodes;
+ 	static uint32_t m_exsitNodes;
+ 	static uint32_t m_numConn;
+ 	static uint32_t m_pktSize;
+
+ 	static std::std::vector<int> m_curInfected;
+
+ 	bool m_infected;
+ 	bool m_connected;
+
+ 	uint16_t m_infectionPort;
+ 	uint32_t m_totalBytes;
+ 	uint32_t m_maxBytes;
+
+ 	std::string m_protocol;
+ 	std::vector< ns3::Ptr<ns3::Socket> > m_onoffSocket;
+
+ 	ns3::TypeId m_typeId;
+ 	ns3::Time m_lastStartTime;
+ 	ns3::Ptr<ns3::RandomVariableStream> m_onTime;       //!< rng for On Time
+    ns3::Ptr<ns3::RandomVariableStream> m_offTime;      //!< rng for Off Time
+ 	ns3::Ptr<ns3::Socket> m_sinkSocket;
+ 	ns3::EventId m_startStopEvent;
+    ns3::EventId m_sendEvent;
+    ns3::DataRate m_cbrRate;    //!< Rate that data is generated
+
+ 	void Write32 (uint8_t *buffer, const uint32_t data);
+    void Read32 (const uint8_t *buffer, uint32_t &data);
+ 
+ private:
+ 	virtual void StartApplication(void);
+ 	virtual void StopApplication(void);
+
+ 	void StartInfectingNodes();
+ 	void Listen(ns3::Ptr<ns3::Socket> socket);
+ 	void CloseAndPrint();
+ 	
+ 	void ConnectionSucceeded(ns3::Ptr<ns3::Socket> socket);
+ 	void ConnectionFailed(ns3::Ptr<ns3::Socket> socket);
+ 	
+ 	void ScheduleStartEvent(uint32_t index);
+ 	void ScheduleStopEvent(uint32_t index);
+ 	
+ 	void SendPacket(uint32_t index);
+ 	
+ 	void StartSending(uint32_t index);
+ 	void StopSending();
+ 	
+ 	void ScheduleNextTx(uint32_t index);
+ 	void CancelEvent();
+ }
