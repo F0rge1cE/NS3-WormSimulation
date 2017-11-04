be7b0bb815619566d6bdcbacc08e12cebe7ab4fa
DoodleNan <doodlenan@outlook.com>
2017-11-04 15:19:42 -0400

    Merge working versions...

--- doodle/ns3-worm.h
+++ doodle/ns3-worm.h
@@ -11,46 +11,27 @@
 #include "ns3/event-id.h"
 #include "ns3/data-rate.h"
 #include "ns3/traced-callback.h"
-
 #include <vector>
 
-//NS_LOG_COMPONENT_DEFINE ("ns3-worm");
-
-/**
- * \class Worm Worm.h
- * \brief Worm application
- */
-class Worm : public ns3::Application
-{
+class Worm : public ns3::Application{
 public:
-    /**
-     * \brief Constructs worm application object
-     */
-    static ns3::TypeId GetTypeId(void);
     Worm();
     ~Worm();
 
     void SetUp(std::string protocol, uint32_t infectionPort);
-
-    ns3::Ipv4Address guessIP(void);
-
-    void SetMaxBytes(uint32_t maxBytes);
-    void PrintState();
     void SetInfected(bool alreadyInfected);
     void SetVulnerable(bool vulnerable);
     void SetName(std::string name);
-
+    
+    static ns3::TypeId GetTypeId(void);
+    static std::vector<int> GetInfectionArray ();
     static void SetX (uint32_t xInt);
     static void SetY (uint32_t yInt);
-    static void SetTotalNodes (uint32_t totalNodes);
     static void SetExistNodes (uint32_t existNodes);
-    static void SetNumConn (uint32_t numConn);
     static void SetPacketSize (uint32_t pktSize);
     static uint32_t GetTotalNodes ();
     static uint32_t GetExistNodes ();
     static uint32_t GetInfectedNodes ();
-    static uint32_t GetNumConn ();
-    static std::vector<int> GetInfectionArray ();
     static void SetNumInfected();
 
 protected:
@@ -59,24 +40,23 @@ protected:
     static uint32_t m_totalInfected;
     static uint32_t m_totalNodes;
     static uint32_t m_existNodes;
-    static uint32_t m_numConn;
+    static uint32_t m_pktSize;
     static std::vector<int> m_curInfected;
-
+    static ns3::Ptr<ns3::UniformRandomVariable> tx;
+    static ns3::Ptr<ns3::UniformRandomVariable> ty;
+    
     bool m_infected;
     bool m_vulnerable;
     bool m_connected;
 
     uint16_t m_infectionPort;
-    static uint32_t m_pktSize;
-    uint32_t m_maxBytes;
     uint32_t m_residualBits; //!< Number of generated, but not sent, bits
     uint32_t m_totalBytes;
-    ns3::Time m_lastStartTime;
-
-
+    
     std::string m_protocol;
     std::string m_name;
 
+    ns3::Time m_lastStartTime;
     ns3::EventId m_startStopEvent;
     ns3::EventId m_sendEvent;
     ns3::TypeId m_typeId;
@@ -86,24 +66,13 @@ protected:
     std::vector< ns3::Ptr<ns3::Socket> > m_onoffSocket;
     ns3::DataRate m_cbrRate;    //!< Rate that data is generated
     ns3::DataRate m_cbrRateFailSafe;
-
-    // static ns3::UniformVariable x;
-    // static ns3::UniformVariable y;
-
-    static ns3::Ptr<ns3::UniformRandomVariable> tx;
-    static ns3::Ptr<ns3::UniformRandomVariable> ty;
-
-    static double x;
-    static double y;
-
-    static uint32_t m_xInt;
-    static uint32_t m_yInt;
+    ns3::Ipv4Address guessIP(void);
+    ns3::TracedCallback<ns3::Ptr<const ns3::Packet> > m_txTrace;
 
     void Write32 (uint8_t *buffer, const uint32_t data);
     void Read32 (const uint8_t *buffer, uint32_t &data);
 
-    ns3::TracedCallback<ns3::Ptr<const ns3::Packet> > m_txTrace;
-
+    
 private:
 //     inherited from Application base class.
     virtual void StartApplication (void);    // Called at time specified by Start
