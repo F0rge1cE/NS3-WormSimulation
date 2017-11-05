0dda4976d93ec23c470af80428145f97338589a7
Jessica Fang <wenxinfang97@gmail.com>
2017-11-05 13:28:57 -0800

    update script

--- p3a.sh
+++ p3a.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-list="TcpNewReno TcpHybla TcpHighSpeed TcpHtcp TcpVegas TcpScalable TcpVeno TcpBic TcpYeah TcpIllinois TcpWestwood TcpWestwoodPlus TcpLedbat"
-for one_thing in $list
-do for ((i=1;i<64;i=i*2))
-do cd "localtion_of_your_ns_3_folder_where_waf_is_located" 
-./waf --run "p1 --nSpokes=$i --protocol=$one_thing" > "localtion_where_you_want_your_output_files_to_be" /$one_thing$i.txt
-done
+# list="TcpNewReno TcpHybla TcpHighSpeed TcpHtcp TcpVegas TcpScalable TcpVeno TcpBic TcpYeah TcpIllinois TcpWestwood TcpWestwoodPlus TcpLedbat"
+msg = "nullmsg"
+list = [1, 2, 4]
+for i in $list
+do cd "scratch" 
+mpirun --np2 ../waf --run "worm --threads=$i"
+mpirun --np2 ../waf --run "worm --threads=$i --nullmsg=$msg"
+# ./waf --run "p1 --nSpokes=$i --protocol=$one_thing" > "localtion_where_you_want_your_output_files_to_be" /$one_thing$i.txt
 done
