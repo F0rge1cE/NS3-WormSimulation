a221564c36830806743af711fc18988febc0900f
WillQiang <33236118+Willqiangyw@users.noreply.github.com>
2017-11-05 16:37:15 -0500

    add in wall clock time, not working as expected

---
+++ qyw/mpiTest.cc
@@ -0,0 +1,480 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Author: Aderinola Gbade-Alabi <aagbade@gmail.com>
+ *         Jared S. Ivey         <jivey@gatech.edu>
+ *         Drew Petry            <drew.petry@gatech.edu>
+ *         Peter Vieira          <pete.vieira@gmail.com>
+ *
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <sys/time.h>
+#include <vector>
+#include <time.h>
+#include <iomanip>
+#include <assert.h>
+
+//#include "ns3/random-variable-stream.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/point-to-point-layout-module.h"
+#include "ns3/stats-module.h"
+
+#include "ns3-worm.h"
+
+//what I added//////////////////////////////////////////////////
+#include "p2pCampusHelper.h"
+//////////////////////////////////////////////////////////////////
+
+// ****** For MPI
+#include "ns3/mpi-interface.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/ipv4-nix-vector-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#ifdef NS3_MPI
+#include <mpi.h>
+#endif
+// ******
+
+
+// ------------ Define worm types    ---------------
+#define TCPWORMTYPE  1
+#define UDPWORMTYPE  2
+#define WORMTYPE     UDPWORMTYPE
+
+// ------------ Define the topology  ---------------
+#define TREES        4
+#define FANOUT1      8
+#define FANOUT2      16
+#define TREELEGPROB  0.85
+
+#define LINKBW       "1Mbps"
+#define HLINKBW      "10Mbps"
+#define BLINKBW      "100Mbps"
+
+// ------------ Worm parameters -----------------------
+#define VULNERABILITY  1.0
+#define SCANRATE       100
+#define SCANRANGE      0
+#define PAYLOAD        1000
+#define NUMCONN        1
+
+// ----------- Simulation settings -------------------
+#define SIMTIME        5
+#define SEEDVALUE      1
+
+// ****** For MPI
+// ----------- MPI settings -------------------
+#define NIX true
+#define NULLMSG false
+#define TRACING false
+// ******
+
+//
+#define PATTERNID 2
+
+using namespace ns3;
+using namespace std;
+typedef struct timeval TIMER_TYPE;
+#define TIMER_NOW(_t) gettimeofday (&_t,NULL);
+#define TIMER_SECONDS(_t) ((double)(_t).tv_sec + (_t).tv_usec * 1e-6)
+#define TIMER_DIFF(_t1, _t2) (TIMER_SECONDS (_t1) - TIMER_SECONDS (_t2))
+
+int main(int argc, char* argv[])
+{
+// ****** For MPI
+#ifdef NS3_MPI
+// ******
+  TIMER_TYPE t0, t1, t2;
+  TIMER_NOW (t0);
+
+
+  GlobalValue::Bind ("SimulatorImplementationType",
+    StringValue ("ns3::DistributedSimulatorImpl"));
+  
+  uint32_t wormtype = WORMTYPE;
+  uint32_t nt = TREES;
+  uint32_t nf1 = FANOUT1;
+  uint32_t nf2 = FANOUT2;
+  string linkbw  = LINKBW;
+  string hlinkbw = HLINKBW;
+  string blinkbw = BLINKBW;
+  uint32_t scanrate = SCANRATE;
+  uint32_t patternId = PATTERNID;
+  uint32_t payload = PAYLOAD;
+  uint32_t seedValue = SEEDVALUE;
+  uint32_t numConn = NUMCONN;
+  double vulnerability = VULNERABILITY;
+  double treelegprob = TREELEGPROB;
+  double simtime = SIMTIME;
+  bool logTop = 0;
+  std::string dataFileName = "p4.data";
+
+  // ****** For MPI
+  // Default configuration
+  bool nix = NIX;
+  bool nullmsg = NULLMSG;
+  bool tracing = TRACING;
+  // ******
+
+  CommandLine cmd;
+  cmd.AddValue ("wormtype",      "Type of worm: UDP or TCP",     wormtype);
+  cmd.AddValue ("trees",         "Number of trees",              nt);
+  cmd.AddValue ("fanout1",       "First fanout of trees",        nf1);
+  cmd.AddValue ("fanout2",       "Second fanout of trees",       nf2);
+  cmd.AddValue ("linkbw",        "Link bandwidth",               linkbw);
+  cmd.AddValue ("hlinkbw",       "HLink bandwidth",              hlinkbw);
+  cmd.AddValue ("blinkbw",       "BLink bandwidth",              blinkbw);
+  cmd.AddValue ("scanrate",      "Scan rate",                    scanrate);
+  cmd.AddValue ("patternId",      "Pattern Id",                  patternId);
+  cmd.AddValue ("payload",       "Payload",                      payload);
+  cmd.AddValue ("seedvalue",     "Seed value for RNG",           seedValue);
+  cmd.AddValue ("vulnerability", "Vulnerability to infection",   vulnerability);
+  cmd.AddValue ("numConn",       "Number of TCP connections",    numConn);
+  cmd.AddValue ("treelegprob",   "Probability of tree legs",     treelegprob);
+  cmd.AddValue ("simtime",       "Simulator time in seconds",    simtime);
+  cmd.AddValue ("logTop",        "Display the topology stats",   logTop);
+  cmd.AddValue ("filename",      "Name of output file",          dataFileName);
+
+  // ****** For MPI
+  cmd.AddValue ("nix", "Enable the use of nix-vector or global routing", nix);
+  cmd.AddValue ("nullmsg", "Enable the use of null-message synchronization", nullmsg);
+  cmd.AddValue ("tracing", "Enable pcap tracing", tracing);
+  // ******
+
+  cmd.Parse (argc,argv);
+
+  // Set the random number generator
+  SeedManager::SetSeed (seedValue);
+  //SeedManager::SetSeed ((int) clock());
+  Ptr<UniformRandomVariable> uv = CreateObject<UniformRandomVariable> ();
+
+  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TcpNewReno::GetTypeId ()));
+
+  // ****** For MPI
+  // Distributed simulation setup; by default use granted time window algorithm.
+  if(nullmsg)
+    {
+      GlobalValue::Bind ("SimulatorImplementationType",
+                         StringValue ("ns3::NullMessageSimulatorImpl"));
+    }
+  else
+    {
+      GlobalValue::Bind ("SimulatorImplementationType",
+                         StringValue ("ns3::DistributedSimulatorImpl"));
+    }
+
+  // Enable parallel simulator with the command line arguments
+  MpiInterface::Enable (&argc, &argv);
+
+  // LogComponentEnable ("PacketSink", LOG_LEVEL_INFO);
+
+  // Get rank and total number of CPUs.
+  uint32_t systemId = MpiInterface::GetSystemId ();
+  uint32_t systemCount = MpiInterface::GetSize ();
+
+  // We only do simulation with 4 processors.
+  if (systemCount != 1 && systemCount != 2 && systemCount != 4)
+    {
+      std::cout << "Only 1, 2 or 4 processors are accepted. Now have " << systemCount << std::endl;
+      return 1;
+    }
+  // ******
+
+
+
+  //what I added////////////////////////////////////////////////
+  uint32_t nInner = 8;
+  uint32_t nChild = 2;
+
+  PointToPointHelper hubInner;
+  hubInner.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+  hubInner.SetChannelAttribute("Delay", StringValue("5ms"));
+
+  PointToPointHelper innerChild;
+  innerChild.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+  innerChild.SetChannelAttribute("Delay", StringValue("8ms"));
+
+  // ****** For MPI
+  // P2P between hubs
+  PointToPointHelper hub2hub_10ms;
+  hub2hub_10ms.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+  hub2hub_10ms.SetChannelAttribute("Delay", StringValue("10ms"));
+
+  PointToPointHelper hub2hub_100ms;
+  hub2hub_100ms.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+  hub2hub_100ms.SetChannelAttribute("Delay", StringValue("10ms"));
+
+  PointToPointHelper hub2hub_500ms;
+  hub2hub_500ms.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+  hub2hub_500ms.SetChannelAttribute("Delay", StringValue("10ms"));
+
+  PointToPointHelper hub2hub_200ms;
+  hub2hub_500ms.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+  hub2hub_500ms.SetChannelAttribute("Delay", StringValue("10ms"));
+
+  // Create nodes
+  PointToPointCampusHelper bomb0(nInner, hubInner, nChild, innerChild, 0%systemCount);
+  PointToPointCampusHelper bomb1(nInner, hubInner, nChild, innerChild, 1%systemCount);
+  PointToPointCampusHelper bomb2(nInner, hubInner, nChild, innerChild, 2%systemCount);
+  PointToPointCampusHelper bomb3(nInner, hubInner, nChild, innerChild, 3%systemCount);
+
+  NetDeviceContainer hubDevice;
+  NetDeviceContainer hub2hub_dev1 = hub2hub_10ms.Install (bomb0.GetHub(), bomb1.GetHub());
+  NetDeviceContainer hub2hub_dev2 = hub2hub_100ms.Install (bomb1.GetHub(), bomb2.GetHub());
+  NetDeviceContainer hub2hub_dev3 = hub2hub_500ms.Install (bomb2.GetHub(), bomb3.GetHub());
+  NetDeviceContainer hub2hub_dev4 = hub2hub_200ms.Install (bomb3.GetHub(), bomb0.GetHub());
+
+  InternetStackHelper stack;
+
+  // Apply Nix Vector
+  if (nix)
+    {
+      std::cout << "Nix Vector Enabled " << std::endl;
+      Ipv4NixVectorHelper nixRouting;
+      stack.SetRoutingHelper (nixRouting); // has effect on the next Install ()
+    }
+
+  // stack.InstallAll ();
+
+  bomb0.InstallStack(stack);
+  bomb1.InstallStack(stack);
+  bomb2.InstallStack(stack);
+  bomb3.InstallStack(stack);
+
+  Ipv4AddressHelper address;
+  address.SetBase("10.1.1.0", "255.255.255.0");
+  bomb0.AssignIpv4Addresses(address);
+
+  // bomb1.InstallStack(stack);
+  address.SetBase("10.2.1.0", "255.255.255.0");
+  bomb1.AssignIpv4Addresses(address);
+
+  // bomb2.InstallStack(stack);
+  address.SetBase("10.3.1.0", "255.255.255.0");
+  bomb2.AssignIpv4Addresses(address);
+
+  // bomb3.InstallStack(stack);
+  address.SetBase("10.4.1.0", "255.255.255.0");
+  bomb3.AssignIpv4Addresses(address);
+
+  address.SetBase("11.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer hub2hub_inter1 = address.Assign(hub2hub_dev1);
+
+  address.SetBase("12.4.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer hub2hub_inter2 = address.Assign(hub2hub_dev2);
+
+  address.SetBase("13.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer hub2hub_inter3 = address.Assign(hub2hub_dev3);
+
+  address.SetBase("14.4.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer hub2hub_inter4 = address.Assign(hub2hub_dev4);
+
+  // ApplicationContainer wormApps; // ???
+  // Worm::SetX (1 + nInner);
+  // Worm::SetY (nInner * nChild);
+  // Worm::SetTotalNodes (nInner * nChild);
+  //Worm::SetNumConn(numConn);
+  Worm::SetPacketSize(payload);
+  uint32_t numVulnerableNodes = 0;
+
+  // Add the worm application to each node.
+  if(systemId == 0%systemCount){
+    for(uint32_t i=0; i < nChild * nInner; i++)
+    {
+      Ptr<Worm> wormApp = CreateObject<Worm> ();
+      //wormApp->SetMaxBytes(50000);
+
+      if (uv->GetValue(0.0, 1.0) <= vulnerability) {
+        wormApp->SetVulnerable (true);
+        numVulnerableNodes++;
+      }
+
+      std::string temp1 = std::to_string(i) + " " + std::to_string(systemId);
+      wormApp->SetName(temp1);
+
+      // Set the initial infected node.
+      if(i==0){
+        wormApp -> SetInfected (true);
+        wormApp -> SetTotalNumOfInfected(1);
+      }
+
+      wormApp->SetStartTime (Seconds (0.0));
+      wormApp->SetStopTime (Seconds (simtime));
+      wormApp->SetPatternId (patternId);
+
+      bomb0.GetChildNode(i)->AddApplication (wormApp);
+      wormApp->SetUp ("ns3::UdpSocketFactory", 5000, systemId);
+    }
+  }
+
+  if(systemId == 1%systemCount){
+    for(uint32_t i=0; i < nChild * nInner; i++)
+    {
+      Ptr<Worm> wormApp = CreateObject<Worm> ();
+      //wormApp->SetMaxBytes(50000);
+
+      if (uv->GetValue(0.0, 1.0) <= vulnerability) {
+        wormApp->SetVulnerable (true);
+        numVulnerableNodes++;
+      }
+
+      std::string temp1 = std::to_string(i) + " " + std::to_string(systemId);
+      wormApp->SetName(temp1);
+
+
+      wormApp->SetStartTime (Seconds (0.0));
+      wormApp->SetStopTime (Seconds (simtime));
+
+      bomb1.GetChildNode(i)->AddApplication (wormApp);
+      wormApp->SetUp ("ns3::UdpSocketFactory", 5000, systemId);
+    }
+  }
+
+  if(systemId == 2%systemCount){
+    for(uint32_t i=0; i < nChild * nInner; i++){
+      Ptr<Worm> wormApp = CreateObject<Worm> ();
+      //wormApp->SetMaxBytes(50000);
+
+      if (uv->GetValue(0.0, 1.0) <= vulnerability) {
+        wormApp->SetVulnerable (true);
+        numVulnerableNodes++;
+      }
+
+      std::string temp1 = std::to_string(i) + " " + std::to_string(systemId);
+      wormApp->SetName(temp1);
+
+
+      wormApp->SetStartTime (Seconds (0.0));
+      wormApp->SetStopTime (Seconds (simtime));
+
+      bomb2.GetChildNode(i)->AddApplication (wormApp);
+      wormApp->SetUp ("ns3::UdpSocketFactory", 5000, systemId);
+    }
+  }
+
+  if(systemId == 3%systemCount){
+    for(uint32_t i=0; i < nChild * nInner; i++){
+      Ptr<Worm> wormApp = CreateObject<Worm> ();
+      //wormApp->SetMaxBytes(50000);
+
+      if (uv->GetValue(0.0, 1.0) <= vulnerability) {
+        wormApp->SetVulnerable (true);
+        numVulnerableNodes++;
+      }
+
+      std::string temp1 = std::to_string(i) + " " + std::to_string(systemId);
+      wormApp->SetName(temp1);
+
+
+      wormApp->SetStartTime (Seconds (0.0));
+      wormApp->SetStopTime (Seconds (simtime));
+
+      bomb3.GetChildNode(i)->AddApplication (wormApp);
+      wormApp->SetUp ("ns3::UdpSocketFactory", 5000, systemId);
+    }
+  }
+    Worm::SetExistNodes(numVulnerableNodes);
+
+/////////////////////////////////////////////////////////////////////
+
+  for (int i = 0; i < 5000; ++i) {
+      ns3::Simulator::Schedule(ns3::Seconds((double)i*.1), &Worm::SetNumInfected);
+  }
+
+  // Populate routing tables.
+  if (!nix)
+  {
+    std::cout << "Using IPv4 Routing!" << std::endl;
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  }
+  // Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  TIMER_NOW (t1);
+  if (simtime != 0)
+    Simulator::Stop(Seconds(simtime));
+  Simulator::Run();
+  TIMER_NOW (t2);
+
+  double percInfected = 100.*(double)Worm::GetInfectedNodes() / (double)(nChild * nInner);
+  double percVulnerable = 100.*(double)numVulnerableNodes/(double)(nChild * nInner);
+  double percInfToVuln = percInfected / percVulnerable;
+  cerr << "Time(s)\tInf(#)\tTot(#)\tConn(#)\tPerc(%)\tVuln(%)\tInf/Vul(%)" << std::endl;
+  cerr << setprecision(3) << Simulator::Now().GetSeconds() << "\t"
+       << Worm::GetInfectedNodes() << "\t"
+       << nChild * nInner << "\t"
+       << Worm::GetNumConn() << "\t"
+       << setprecision(4) << percInfected << "\t"
+       << setprecision(4) << percVulnerable << "\t"
+       << setprecision(4) << percInfToVuln*100. << "\t"
+       << std::endl;
+
+
+  std::vector<int> infectionArray = Worm::GetInfectionArray();
+  for (int i = 0; i < 5; ++i) {
+    infectionArray.push_back(Worm::GetInfectedNodes());
+  }
+
+  //-------------------------
+  //    WRITE DATA TO FILE
+  //-------------------------
+  //"#Nodes %Vul %Inf #inf #Conn Time"
+  // Write results to data file
+  std::ofstream dataFile;
+  dataFile.open(dataFileName.c_str(), std::fstream::out | std::fstream::app);
+  assert(dataFile.is_open());
+  dataFile << "0.0" << "\t"
+           << Worm::GetInfectedNodes() << "\t"
+           << nChild * nInner << "\t"
+           << percVulnerable << "\t"
+           << percInfected << "\t"
+           << Worm::GetNumConn() << "\t"
+           << payload << "\t"
+           << "\n";
+  for (size_t i = 0; i < infectionArray.size(); ++i) {
+      dataFile << (float)i*0.1 << "\t"
+               << infectionArray.at(i) << "\t"
+               << "\n";
+  }
+  dataFile  << "\n";
+  dataFile.close();
+  //flowmon->SerializeToXmlFile ("p4.flowmon", false, false);
+  Simulator::Destroy();
+
+  // ****** For MPI
+  // Exit the MPI execution environment
+  MpiInterface::Disable ();
+  double d1 = TIMER_DIFF (t1, t0), d2 = TIMER_DIFF (t2, t1);
+  std::cout << "-----" << std::endl << "Runtime Stats:" << std::endl;
+  std::cout << "Simulator init time: " << d1 << std::endl;
+  std::cout << "Simulator run time: " << d2 << std::endl;
+  std::cout << "Total elapsed time: " << d1 + d2 << std::endl;
+  
+  return 0;
+
+#else
+  NS_FATAL_ERROR ("Can't use distributed simulator without MPI compiled in");
+#endif
+// ******
+
+}
--- qyw/ns3-worm.cc
+++ qyw/ns3-worm.cc
@@ -37,11 +37,14 @@ ns3::Ptr<ns3::UniformRandomVariable> Worm::tx = ns3::CreateObject<ns3::UniformRa
 ns3::Ptr<ns3::UniformRandomVariable> Worm::ty = ns3::CreateObject<ns3::UniformRandomVariable> ();
 uint32_t Worm::m_xInt = 256;
 uint32_t Worm::m_yInt = 256;
-uint32_t Worm::m_totalInfected = 1;
+uint32_t Worm::m_totalInfected = 0;
 uint32_t Worm::m_existNodes = 0;
 uint32_t Worm::m_totalNodes = 0;
 uint32_t Worm::m_numConn = 1;
 uint32_t Worm::m_pktSize = 512;
+uint32_t Worm::m_patternId = 0;
+double Worm::m_chooseLocalPercent = 0.65;
+
 std::vector<int> Worm::m_curInfected;
 
 ns3::TypeId Worm::GetTypeId(void)
@@ -89,6 +92,7 @@ Worm::Worm()
     m_connected(false),
     m_residualBits(0),
     m_totalBytes(0),
+    m_addressForPattern3(0),
     m_lastStartTime(ns3::Seconds(0)),
     m_sinkSocket(0)
 {
@@ -104,6 +108,11 @@ Worm::Worm()
 Worm::~Worm()
 {
   NS_LOG_FUNCTION (this);
+}
+
+// Modified for MPI
+void Worm::SetTotalNumOfInfected (uint32_t num){
+  m_totalInfected = num;
 }
 
 void Worm::SetInfected(bool alreadyInfected)
@@ -156,6 +165,11 @@ void Worm::SetNumConn (uint32_t numConn)
   m_numConn = numConn;
 }
 
+void Worm::SetPatternId (uint32_t patternId)
+{
+  m_patternId = patternId;
+}
+
 uint32_t Worm::GetTotalNodes ()
 {
   return m_totalNodes;
@@ -176,10 +190,11 @@ uint32_t Worm::GetNumConn ()
   return m_numConn;
 }
 
-void Worm::SetUp(std::string protocol, uint32_t infectionPort)
+void Worm::SetUp(std::string protocol, uint32_t infectionPort,uint32_t subnetId)
 {
   m_protocol = protocol;
   m_infectionPort = infectionPort;
+  m_subnetId = subnetId;
   m_typeId = ns3::TypeId::LookupByName(m_protocol);
 
   // Sink socket
@@ -215,15 +230,47 @@ ns3::Ipv4Address Worm::guessIP()
   //what I added
   Ptr<UniformRandomVariable> uv = CreateObject<UniformRandomVariable> ();
 
-  double i = uv->GetValue(1.0, 2.0);
-  double j = uv->GetValue(1.0, 9.0);
-  double k = uv->GetValue(1.0, 3.0);
+  double i, j, k;
+  if(m_patternId == 0)
+  {
+    i = uv->GetValue(1.0, 5.0);
+    j = uv->GetValue(1.0, 25.0);
+    // k = uv->GetValue(1.0, 3.0);
+    k = 2;
+  }
+
+  else if (m_patternId == 1)
+  {
+    i = (double) m_subnetId;
+    if(uv->GetValue(0.0, 1.0) > m_chooseLocalPercent){
+      while((uint32_t) i == m_subnetId){
+      i = uv->GetValue(1.0, 5.0);
+      }
+    }
+    j = uv->GetValue(1.0, 25.0);
+    // k = uv->GetValue(1.0, 3.0);
+    k = 2;
+  }
+
+  else if(m_patternId == 2)
+  {
+    // i = m_addressForPattern3 / 2 / 24 + 1;
+    i =  m_addressForPattern3 / 24 + 1;
+    j = (m_addressForPattern3 ) % 24 + 1;
+    // k = m_addressForPattern3 % 2 + 1;
+    k = 2;
+    m_addressForPattern3++;
+
+    // if(m_addressForPattern3 == 4 * 24)
+      // m_addressForPattern3 = 0;
+  }
 
   char buff[13];
-  sprintf(buff, "10.%d.%d.%d", (uint32_t)i, (uint32_t)j, ((uint32_t)(k) * 2 + 2));
-  std::cout << buff << std::endl;
+  sprintf(buff, "10.%d.%d.%d", (uint32_t)i, (uint32_t)j, (uint32_t)k);
+  // std::cout << "Guess:" << buff << std::endl;
   ns3::Ipv4Address address = ns3::Ipv4Address(buff);
   return address;
+
 }
 
 void
@@ -384,9 +431,13 @@ void Worm::Listen(ns3::Ptr<ns3::Socket> socket)
             {
               m_infected = true;
               m_totalInfected++;
-              std::cerr << m_totalInfected << " This worked! sort of" << std::endl;
+              std::cerr << m_totalInfected << " (Worm, CPU) = " << m_name << std::endl;
 
-              if (m_totalInfected >= m_existNodes) ns3::Simulator::Stop();
+              if (m_totalInfected >= m_existNodes)
+              {
+                // ns3::Simulator::Stop(); // Remove to prevent early-stopping of simulatior!
+              }
+
 
               StartInfectingNodes();
             }
