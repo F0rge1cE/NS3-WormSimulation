796166cfff72e192d0b95b9e66b924a90c0cd331
WillQiang <33236118+Willqiangyw@users.noreply.github.com>
2017-11-06 15:49:18 -0500

    Add files via upload

---
+++ ns3-worm.h
@@ -0,0 +1,138 @@
+
+#ifndef WORM_APPLICATION_H
+#define WORM_APPLICATION_H
+
+#include "ns3/core-module.h"
+#include "ns3/application.h"
+#include "ns3/internet-module.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/socket.h"
+#include "ns3/event-id.h"
+#include "ns3/data-rate.h"
+#include "ns3/traced-callback.h"
+
+#include <vector>
+
+//NS_LOG_COMPONENT_DEFINE ("ns3-worm");
+
+/**
+ * \class Worm Worm.h
+ * \brief Worm application
+ */
+class Worm : public ns3::Application
+{
+public:
+    /**
+     * \brief Constructs worm application object
+     */
+    static ns3::TypeId GetTypeId(void);
+    Worm();
+    ~Worm();
+
+    void SetUp(std::string protocol, uint32_t infectionPort, uint32_t subnetId);
+
+    ns3::Ipv4Address guessIP(void);
+
+    // void SetMaxBytes(uint32_t maxBytes);
+    void PrintState();
+    void SetInfected(bool alreadyInfected);
+    void SetVulnerable(bool vulnerable);
+    void SetName(std::string name);
+
+    void SetTotalNumOfInfected (uint32_t num);
+    static void GetCurrentStatus();
+    static uint32_t GetSysId();
+    static void SetSysId(uint32_t id);
+
+
+    // static void SetX (uint32_t xInt);
+    // static void SetY (uint32_t yInt);
+    // static void SetTotalNodes (uint32_t totalNodes);
+    // static void SetExistNodes (uint32_t existNodes);
+    static void SetNumConn (uint32_t numConn);
+    static void SetPacketSize (uint32_t pktSize);
+    static void SetPatternId (uint32_t patternId);
+    // static uint32_t GetTotalNodes ();
+    // static uint32_t GetExistNodes ();
+    static uint32_t GetInfectedNodes ();
+    static uint32_t GetNumConn ();
+    static std::vector<int> GetInfectionArray ();
+    static void SetNumInfected();
+
+protected:
+    void DoDispose (void);
+
+    static uint32_t m_systemID;
+
+    static uint32_t m_totalInfected;
+    // static uint32_t m_totalNodes;
+    // static uint32_t m_existNodes;
+    static uint32_t m_numConn;
+    static std::vector<int> m_curInfected;
+    static double m_chooseLocalPercent;
+
+    bool m_infected;
+    bool m_vulnerable;
+    bool m_connected;
+
+    uint16_t m_infectionPort;
+    uint32_t m_subnetId;
+    static uint32_t m_pktSize;
+    // uint32_t m_maxBytes;
+    uint32_t m_residualBits; //!< Number of generated, but not sent, bits
+    uint32_t m_totalBytes;
+    uint32_t m_addressForPattern3;
+    static uint32_t m_patternId;
+    ns3::Time m_lastStartTime;
+
+
+    std::string m_protocol;
+    std::string m_name;
+
+    // ns3::EventId m_startStopEvent;
+    ns3::EventId m_sendEvent;
+    ns3::TypeId m_typeId;
+    // ns3::Ptr<ns3::RandomVariableStream> m_onTime;       //!< rng for On Time
+    ns3::Ptr<ns3::RandomVariableStream> m_offTime;      //!< rng for Off Time
+    ns3::Ptr<ns3::Socket> m_sinkSocket;
+    std::vector< ns3::Ptr<ns3::Socket> > m_onoffSocket;
+    ns3::DataRate m_cbrRate;    //!< Rate that data is generated
+    ns3::DataRate m_cbrRateFailSafe;
+
+    // static ns3::UniformVariable x;
+    // static ns3::UniformVariable y;
+
+    // static ns3::Ptr<ns3::UniformRandomVariable> tx;
+    // static ns3::Ptr<ns3::UniformRandomVariable> ty;
+
+    // static double x;
+    // static double y;
+
+    // static uint32_t m_xInt;
+    // static uint32_t m_yInt;
+
+    void Write32 (uint8_t *buffer, const uint32_t data);
+    void Read32 (const uint8_t *buffer, uint32_t &data);
+
+    ns3::TracedCallback<ns3::Ptr<const ns3::Packet> > m_txTrace;
+
+private:
+//     inherited from Application base class.
+    virtual void StartApplication (void);    // Called at time specified by Start
+    virtual void StopApplication (void);     // Called at time specified by Stop
+    void StartInfectingNodes();
+    void Listen(ns3::Ptr<ns3::Socket> socket);
+    // void CloseAndPrint();
+    void ConnectionSucceeded(ns3::Ptr<ns3::Socket> socket);
+    void ConnectionFailed(ns3::Ptr<ns3::Socket> socket);
+    void ScheduleStartEvent(uint32_t index);
+    // void ScheduleStopEvent();
+    void SendPacket(uint32_t index);
+    void StartSending(uint32_t index);
+    // void StopSending();
+    void ScheduleNextTx(uint32_t index);
+    // void CancelEvents();
+};
+
+#endif // WORM_APPLICATION_H
--- p2pCampusHelper.cc
+++ p2pCampusHelper.cc
@@ -20,13 +20,14 @@
 #include <sstream>
 
 #include "ns3/log.h"
-#include "ns3/point-to-point-star.h"
 #include "ns3/constant-position-mobility-model.h"
 
 #include "ns3/node-list.h"
 #include "ns3/point-to-point-net-device.h"
 #include "ns3/vector.h"
 #include "ns3/ipv6-address-generator.h"
+
+#include "p2pCampusHelper.h"
 
 using namespace ns3;
 
@@ -37,12 +38,14 @@ PointToPointCampusHelper::PointToPointCampusHelper(uint32_t maxInner, uint32_t m
 PointToPointCampusHelper::PointToPointCampusHelper (uint32_t nInner,
                               PointToPointHelper hub2InnerHelper,
                               uint32_t nChild,
-                              PointToPointHelper inner2ChildHelper){
-  m_hub.Create (1);
-  m_inner.Create (nInner);
-  m_child.Create (nChild * nInner);
+                              PointToPointHelper inner2ChildHelper,
+                              uint32_t systemID){
+  m_hubPtr = CreateObject<Node> (systemID);
+  m_hub.Add (m_hubPtr);
+  m_inner.Create (nInner, systemID);
+  m_child.Create (nChild * nInner, systemID);
 
-  for (uint32_t i = 0; i < m_innerNodes.GetN (); ++i)
+  for (uint32_t i = 0; i < m_inner.GetN (); ++i)
     {
       NetDeviceContainer ndHub2Inner = hub2InnerHelper.Install (m_hub.Get (0), m_inner.Get (i));
       m_hubDevices.Add (ndHub2Inner.Get (0));
@@ -60,81 +63,93 @@ PointToPointCampusHelper::~PointToPointCampusHelper() {
 
 }
 
-Ptr<Node> 
+Ptr<Node>
 PointToPointCampusHelper::GetHub () const{
   return m_hub.Get (0);
 }
 
-Ptr<Node> 
+Ptr<Node>
 PointToPointCampusHelper::GetInnerNode (uint32_t i) const{
   return m_inner.Get (i);
 }
 
-Ptr<Node> 
+Ptr<Node>
 PointToPointCampusHelper::GetChildNode (uint32_t i) const{
   return m_child.Get (i);
 }
 
-Ipv4Address 
+Ipv4Address
 PointToPointCampusHelper::GetHubIpv4Address (uint32_t i) const{
   return m_hubInterfaces.GetAddress (i);
 }
 
-Ipv4Address 
+Ipv4Address
 PointToPointCampusHelper::GetInnerIpv4Address (uint32_t i) const{
   return m_innerInterfaces.GetAddress (i);
 }
 
-Ipv4Address 
+Ipv4Address
 PointToPointCampusHelper::GetChildIpv4Address (uint32_t i) const{
   return m_childInterfaces.GetAddress (i);
 }
 
-Ipv6Address 
+Ipv6Address
 PointToPointCampusHelper::GetHubIpv6Address (uint32_t i) const{
   return m_hubInterfaces6.GetAddress (i, 1);
 }
 
-Ipv6Address 
+Ipv6Address
 PointToPointCampusHelper::GetInnerIpv6Address (uint32_t i) const{
   return m_innerInterfaces6.GetAddress(i, 1);
 }
 
-Ipv6Address 
+Ipv6Address
 PointToPointCampusHelper::GetChildIpv6Address (uint32_t i) const{
   return m_childInterfaces6.GetAddress(i, 1);
 }
 
-void 
+void
 PointToPointCampusHelper::InstallStack (InternetStackHelper stack){
   stack.Install(m_hub);
   stack.Install(m_inner);
   stack.Install(m_child);
 }
 
-void 
+void
 PointToPointCampusHelper::AssignIpv4Addresses (Ipv4AddressHelper address){
   uint32_t nChild = m_child.GetN () / m_inner.GetN ();
-  
+
   for (uint32_t i = 0; i < m_inner.GetN (); ++i)
     {
       m_hubInterfaces.Add (address.Assign (m_hubDevices.Get (i)));
       m_innerInterfaces.Add (address.Assign (m_innerDevices.Get (i * (nChild + 1))));
+
+      address.NewNetwork ();
+      // std::cout << m_hubInterfaces.GetAddress(i) << std::endl;
+      // std::cout << m_innerInterfaces.GetAddress(3*i) << std::endl;
+
       for (uint32_t j = 0; j < nChild; ++j){
         m_innerInterfaces.Add (address.Assign (m_innerDevices.Get (i * (nChild + 1) + (j + 1))));
         m_childInterfaces.Add (address.Assign (m_childDevices.Get (i * nChild + j)));
+
+        address.NewNetwork ();
+        // std::cout << m_innerInterfaces.GetAddress(i*3+j+1) << std::endl;
+        // std::cout << m_childInterfaces.GetAddress(i*2 + j) << std::endl;
+
       }
-      address.NewNetwork ();
+      // address.NewNetwork ();
     }
+
 }
 
+
+
+uint32_t
 PointToPointCampusHelper::InnerCount() const{
 	return m_inner.GetN ();
 }
 
-
+uint32_t
 PointToPointCampusHelper::ChildCount() const{
 	return m_child.GetN ();
 }
-
-
